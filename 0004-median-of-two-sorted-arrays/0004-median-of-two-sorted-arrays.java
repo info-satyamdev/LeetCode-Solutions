de1484a8bdef318f3585f69b0f77ff935783ae96
Satyam <info.satyam.dev@gmail.com>
2026-08-19 01:34:37 +0530

    Time: 1 ms (100%), Space: 48.8 MB (52.62%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,14 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i = 0;
+        int k = 0;
+        int j = 0;
         int solutionArraySize = nums1.length + nums2.length;
-        int [] solutionArray = new int[solutionArraySize];
-        for(int i = 0; i<nums1.length; i++){
-            solutionArray[i] = nums1[i];
+        int [] solutionArray = new int [solutionArraySize];
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                solutionArray[k] = nums1[i];
+                i++;
+            } else {
+                solutionArray[k] = nums2[j];
+                j++;
+            }
+            k++;
         }
-        for(int i = 0; i<nums2.length; i++){
-            solutionArray[nums1.length + i] = nums2[i];
+        while (i < nums1.length) {
+            solutionArray[k] = nums1[i];
+            i++;
+            k++;
         }
-        Arrays.sort(solutionArray);
+        while (j < nums2.length) {
+            solutionArray[k] = nums2[j];
+            j++;
+            k++;
+        }
         int medianIndex = solutionArraySize /2;
         double median = 0;
         if(solutionArraySize%2 == 0){
